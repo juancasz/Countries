4c659f0e7c421dac61b1889b1e980b0477eed41f
Juan Jose <juancasz0208@gmail.com>
2020-08-26 17:00:22 -0500

    heroku deployment

---
+++ build/precache-manifest.d55653063dece28e16a04b830ca40f72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484636ae5eadeff3a86710afa8742ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e37804e8430833f97902",
+    "url": "/static/js/2.5236e4eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5236e4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bd4182e3953df63963",
+    "url": "/static/js/main.25fcb21b.chunk.js"
+  },
+  {
+    "revision": "f4e3d70bd5967adc144d",
+    "url": "/static/js/runtime-main.a5f11eaf.js"
+  }
+]);
